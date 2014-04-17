abb261ebd937069ae1c7b2c594a37c51a8ac79a1
James <james.tan@ceosoftcenters.com>
2014-04-17 12:13:52 +0800

    no message

--- lib/ceo_auto_time_tracker_patch.rb
+++ lib/ceo_auto_time_tracker_patch.rb
@@ -14,6 +14,7 @@ module CEOAutoTimeTrackerPatch
       Rails.logger.info params[:status_id]
       if params[:status_id] == '2'
         Rails.logger.info '111111111'
+      end
     end
   end
 end
