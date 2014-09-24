726721a2e4930b34045a61720c538665ea0618c4
James <james.tan@ceosoftcenters.com>
2014-09-24 16:32:19 +0800

    no message

--- lib/ceo_auto_time_tracker_patch.rb
+++ lib/ceo_auto_time_tracker_patch.rb
@@ -1,3 +1,6 @@
+# status id:
+#	1 -- new;
+#	2 -- in progress;
 module CEOAutoTimeTrackerPatch
   def self.included(base) # :nodoc:
     base.send(:include, InstanceMethods)
@@ -10,10 +13,21 @@ module CEOAutoTimeTrackerPatch
   module InstanceMethods
     def update_with_time_track
       update_without_time_track
-      Rails.logger.info '0000000000000000'
-      Rails.logger.info params[:status_id]
-      if params[:status_id] == '2'
-        Rails.logger.info '111111111'
+      
+      if params[:status_id] == '2' # status_id is new status, changed from 'any' -> 'in process'
+      	Rails.logger.info '111111111111111'
+      	
+        # get time tracker and start it
+        @time_tracker = TimeTracker.new(:issue_id => params[:id], :activity_id => 9, :project_id => params[:project_id])
+        
+        #todo: show error msg if already started
+	    @time_tracker.start
+	    
+	  else
+	  	
+	  	@time_tracker = TimeTracker.where(:issue_id => params[:id]).first;
+	  	
+	  	@time_tracker.stop if @time_tracker != nil
       end
     end
   end
